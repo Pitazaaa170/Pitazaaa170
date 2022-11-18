97da4f54bb033fda5ff12afe37a1b899c56d99b4
gleb032 <fandeev.business@gmail.com>
2022-11-18 20:21:09 +0300

    Initial Commit

---
+++ Pitaza170/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pitaza170
+//
+//  Created by Глеб Фандеев on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
